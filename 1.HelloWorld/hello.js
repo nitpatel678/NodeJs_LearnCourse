d71a77388f5be7634e5ecb0b06d68eece5941623
Nitin Patel <nitpatel678@gmail.com>
2025-02-05 00:52:08 +0530

    Update

--- 1.HelloWorld/hello.js
+++ 1.HelloWorld/hello.js
@@ -1,3 +1,10 @@
 console.log("Hey there Node");
 // But we cant run dom manipulation and window functionality of javascript 
 // because that has been removed from node for c++
+
+const math = require("./math");
+// const {add, sub} = require("./math");
+// console.log("The value of the number is", sub(36,33));
+
+console.log("The value of the number is", math.sub(36,33));
+console.log("The value of the number is", math.add(36,33));
---
+++ 1.HelloWorld/math.js
@@ -0,0 +1,16 @@
+function add (a,b){
+    return a + b;
+}
+
+function sub (a,b){
+    return a - b;
+}
+
+// module.exports = add;
+
+// To add multiple function
+module.exports={
+    add,sub
+}
+
+// exports.add = (a,b) => a+b;
